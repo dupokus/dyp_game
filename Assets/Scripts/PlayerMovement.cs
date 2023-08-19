150b2e7094d72ee6afdaf79b39ba8332b430babe
Mykhailo Kalchenko <mihailkalchenko01@gmail.com>
2023-08-19 22:47:12 +0300

    added idle and running animations, and ability to jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private PlayerController characterController;
+    [SerializeField] private Animator characterAnimator;
 
     float horizontalMove = 0f;
     float runSpeed = 40f;
+    bool jump = false;
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        characterAnimator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     private void FixedUpdate()
     {
         // moving player
-        characterController.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        characterController.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;   
     }
 }       
